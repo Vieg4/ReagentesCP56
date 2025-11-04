7132dfe273c64828d5f0fb88b08e83fd405273af
Gustavo <gvmlopes@gmail.com>
2025-11-04 15:56:40 -0300

    Criando o projeto e configurando

---
+++ src/test/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/ReagentesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esph.ddd.reagentes.reagentes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReagentesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
