93866a5761ef7276007ef19d8810c94af6d2fd2b
Gustavo <gvmlopes@gmail.com>
2025-11-04 16:08:26 -0300

    Configurando os status

--- src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/StatusReagente.java
+++ src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/StatusReagente.java
@@ -1,23 +1,10 @@
 package br.com.fiap.esph.ddd.reagentes.reagentes_api.domain.model;
 
 public enum StatusReagente {
-    /**
-     * O caso esta em andamento.
-     */
-    ATIVO,
-
-    /**
-     * O caso esta em fase de analise ou consulta inicial.
-     */
-    Consultoria,
-
-    /**
-     * O caso foi concluido e esta aguardando arquivamento.
-     */
-    Fechado,
-
-    /**
-     * O caso foi consluido e arquivado.
-     */
-    Arquivado
+    Quarentena,
+    Liberado,
+    Em_uso,
+    Vencido,
+    Reprovado_controle_qualidade,
+    Descartado
 }
