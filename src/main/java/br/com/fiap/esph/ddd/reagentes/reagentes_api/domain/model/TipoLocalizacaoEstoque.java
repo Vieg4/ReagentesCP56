6409c22d0355229da9a32149e5ef234593e688eb
Gustavo <gvmlopes@gmail.com>
2025-11-04 17:34:34 -0300

    Criando os Status gerais

--- src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/TipoLocalizacaoEstoque.java
+++ src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/TipoLocalizacaoEstoque.java
@@ -1,5 +1,9 @@
 package br.com.fiap.esph.ddd.reagentes.reagentes_api.domain.model;
 
-public class TipoLocalizacaoEstoque {
-
+public enum TipoLocalizacaoEstoque {
+    Refrigerador,
+    Freezer_minus20,
+    Freezer_minus80,
+    Prateleira_Ambiente,
+    Outro;
 }
--- src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/TipoMovimentacao.java
+++ src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/TipoMovimentacao.java
@@ -1,5 +1,10 @@
 package br.com.fiap.esph.ddd.reagentes.reagentes_api.domain.model;
 
-public class TipoMovimentacao {
-
+public enum TipoMovimentacao {
+    Entrada_nota,
+    Saida_uso_analisador,
+    Saida_descarte_vencimento,
+    Saida_descarte_controle_qualidade,
+    Ajuste_inventario_positivo,
+    Ajuste_inventario_negativo;
 }
