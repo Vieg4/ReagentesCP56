b74120cace6d9b896fcd79b778e8da464b47538e
Gustavo <gvmlopes@gmail.com>
2025-11-04 16:28:01 -0300

    Iniciando o Fabricante.java

--- src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/Fabricante.java
+++ src/main/java/br/com/fiap/esph/ddd/reagentes/reagentes_api/domain/model/Fabricante.java
@@ -1,5 +1,30 @@
 package br.com.fiap.esph.ddd.reagentes.reagentes_api.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Fabricante {
 
+    private UUID id;
+
+    private String nomeOficial;
+    
+    private String nomeFantasia;
+
+    private String cnpj;
+
+    private String paisOrigem;
+
+    private List<Reagente> reagentes = new ArrayList<>() ;
+
+
+
+    public void adicinarReagente (Reagente reagente) {
+
+    }
+
+    public void removerReagente (Reagente reagente) {
+
+    }
 }
